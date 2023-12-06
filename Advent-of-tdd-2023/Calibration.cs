6f0a06da3f0b87d5763c65410a77a68116aaee02
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-06 08:27:53 +0000

    Update Calibration.cs

--- Advent-of-tdd-2023/Calibration.cs
+++ Advent-of-tdd-2023/Calibration.cs
@@ -8,6 +8,11 @@ namespace AdventOfCodeTDD
     {
         public static void Main()
         {
+           EngineDetails data=new EngineDetails();
+           data.ReadData("Engine.txt");
+           Console.WriteLine("Sum of Valid Parts is {0}",data.SumOfEngineParts();
+           Console.WriteLine("Sum of Gear Ratios is {0}",data.SumOfGearRatios();
+                             
         }
     }
 }
