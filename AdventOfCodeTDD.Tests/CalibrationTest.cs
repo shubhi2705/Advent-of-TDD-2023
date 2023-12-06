f69cb12bf5a20e577a884625351cd19a5f4ea1c6
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-06 09:19:40 +0000

    Update CalibrationTest.cs

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -43,5 +43,42 @@ namespace AdventOfCodeTDD.Tests
             check.SymbolsInRow(0,"$*123...*456..");
             Assert.AreEqual(3,check.parts.Count());
         }
-    }
+
+        [Test]
+        public void ValidPartNumberSumDueToTopSymbol()
+        {
+            EngineDetails check=new EngineDetails();
+            check.SymbolsInRow(0,"$*123...*456..");
+            check.PartNumbersInRow(0,"$*123...*456..");
+            check.SymbolsInRow(1,"..123....456..");
+            check.PartNumbersInRow(1,"..123....456..");
+            var value=check.SumofEngineParts();
+            Assert.AreEqual(1158,value);
+        }
+
+        [Test]
+        public void ValidPartNumberSumDueToBottomSymbol()
+        {
+            EngineDetails check=new EngineDetails();
+            check.SymbolsInRow(0,"..123....456..");
+            check.PartNumbersInRow(0,"..123....456..");
+            check.SymbolsInRow(1,".*123....456..");
+            check.PartNumbersInRow(1,".*123....456..");
+            var value=check.SumofEngineParts();
+            Assert.AreEqual(246,value);
+        
+        }
+
+        [Test]
+        public void ValidPartNumberSumDueToDiagonalSymbol()
+        {
+            EngineDetails check=new EngineDetails();
+            check.SymbolsInRow(0,"..123....456..");
+            check.PartNumbersInRow(0,"..123....456..");
+            check.SymbolsInRow(1,".*123....456..");
+            check.PartNumbersInRow(1,".*123....456..");
+            var value=check.SumofEngineParts();
+            Assert.AreEqual(246,value);
+        }
+    
 }
