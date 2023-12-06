9cc865d4f782026ac92002a683d1955d32135e6a
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-06 08:56:12 +0000

    Update CalibrationTest.cs

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -6,11 +6,26 @@ namespace AdventOfCodeTDD.Tests
     public class CalibrationTest
     {
         [Test]
+        public void FileNotFoundException()
+        {
+            EngineDetails check=new EngineDetails();
+            Assert.Throws<FileNotFoundException>(()=>check.Readdata("abc.txt"));       
+        }
+        [Test]
         public void NoSymbolsInRow()
         {
             EngineDetails check=new EngineDetails();
             check.SymbolsInRow(0,"988..345..");
             Assert.AreEqual(0,check.symbols.Count());
+            
+        }
+
+        [Test]
+        public void NoNumbersInRow()
+        {
+            EngineDetails check=new EngineDetails();
+            check.NumbersInRow(0,"$*..$..");
+            Assert.AreEqual(0,check.parts.Count());
             
         }
         [Test]
