5d770543fc67238c63d13add738f1c35c7aa8832
Shubhi Singhal <shubhisinghal2705@gmail.com>
2023-12-07 17:54:10 +0530

    Day-3 solution

---
+++ Advent-of-tdd-2023/Program.cs
@@ -0,0 +1,19 @@
+﻿using Advent_of_tdd_2023;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+namespace AdventOfCodeTDD
+{
+   public class Program
+    {
+        public static void Main()
+        {
+           EngineDetail data=new EngineDetail();
+           data.ReadData("myfile.txt");
+           Console.WriteLine("Sum of Valid Parts is {0}",data.SumofEngineParts());
+           Console.WriteLine("Sum of Gear Ratios is {0}",data.SumofGearRatios());
+                             
+        }
+    }
+}
