e69824e53fe3e9c223a91552da2f874f6b377629
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-07 07:45:39 +0000

    Update CamelGames.cs

--- Advent-of-tdd-2023/CamelGames.cs
+++ Advent-of-tdd-2023/CamelGames.cs
@@ -3,5 +3,13 @@ using System.Text.RegularExpressions;
 namespace AdventOfCodeTDD
 {
   public class CamelGames{
+
+    public int bidAmount;
+    public int rank;
+    public string HandValue;
+
+    public List<string> hands=new List<string>();
+    public List<int> bids=new List<int>();
+    public List<CamelGames> hands=new List<CamelGames>();
   }
 }
