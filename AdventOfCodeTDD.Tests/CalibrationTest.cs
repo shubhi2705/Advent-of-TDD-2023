38511685588c404f9da80bfb865e49b071b87024
Shubhi Singhal <shubhisinghal2705@gmail.com>
2023-12-07 15:32:29 +0530

    Added unit test methods for day-7

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -6,14 +6,82 @@ namespace AdventOfCodeTDD.Tests
     public class CalibrationTest
     {
         [Test]
-        public void Test1()
+        public void check_whether_given_input_is_FiveOfaKind_type()
         {
             
         }
         [Test]
-        public void Test2()
+        public void check_whether_given_input_is_FourOfaKind_type()
         {
 
         }
+        [Test]
+        public void check_whether_given_input_is_FullHouse_type()
+        {
+
+        }
+        [Test]
+        public void check_whether_given_input_is_ThreeOfAKind_type()
+        {
+
+        }
+        [Test]
+        public void check_whether_given_input_is_TwoPair_type()
+        {
+
+        }
+        [Test]
+        public void check_whether_given_input_is_OnePair_type()
+        {
+
+        }
+        [Test]
+        public void check_whether_given_input_is_HighCard_type()
+        {
+
+        }
+
+        [Test]
+        public void check_whether_33332_is_stronger_than_2AAAAA()
+        {
+
+        }
+
+        [Test]
+        public void check_whether_77888_is_stronger_than_77788()
+        {
+
+        }
+
+        [Test]
+        public void check_ranks_of_given_input_32T3K_T55J5()
+        {
+
+        }
+
+        [Test]
+        public void check_whether_input_is_empty()
+        {
+
+        }
+
+        [Test]
+        public void check_whether_input_is_invalid()
+        {
+
+        }
+
+        [Test]
+        public void calculate_bid_rank_multiplication_for_given_input()
+        {
+
+        }
+
+        [Test]
+        public void calculate_total_winning_for_given_input()
+        {
+
+        }
+
     }
 }
