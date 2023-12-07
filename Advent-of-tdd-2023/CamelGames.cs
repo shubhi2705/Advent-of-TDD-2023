4e4a9d30f2302c248e633230a1f8da853b6562df
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-07 08:05:42 +0000

    Update CamelGames.cs

--- Advent-of-tdd-2023/CamelGames.cs
+++ Advent-of-tdd-2023/CamelGames.cs
@@ -51,5 +51,60 @@ namespace AdventOfCodeTDD
             throw new FileNotFoundException();
         }
     }
+
+  public int CheckRank()
+  {
+    int sum=0;
+    foreach(var game in games)
+    {
+      string data=game.Handvalue;
+      Dictionary<char,int> alphabetCount=new Dictionary<char,int>();
+      for(int i=0;i<data.Length;i++)
+      {
+        if(alphabetCount.ContainsKey(data[i]))
+           {
+             alphabetCount[data[i]]++;
+           }
+           else
+           {
+             alphabetCount[data[i]]=1;
+           }   
+      }
+
+      switch(alphabetCount.Count)
+      {
+          case1:
+          game.rank=5;
+          break;
+        case 2:
+          if(alphabetCount.ContainsValue(4))
+          { game.rank=4;}
+          else
+          {
+            game.rank=3;
+          }
+          break;
+          case 3:
+          if(alphabetCount.ContainsValue(3))
+          { game.rank=3;}
+          else
+          {game.rank=2;
+          }
+          break;
+        case 4: 
+          if(alphabetCount.ContainsValue(2))
+          game.rank=2;
+          break
+          case 5:
+          game.rank=1;
+          break;
+      }
+    }
+      foreach(var game in games)
+      {
+        sum=sum+(game.rank * game.bidAmount);
+      }
+    }
+  
 }
 }
