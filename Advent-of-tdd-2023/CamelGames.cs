bf61b5b0ce91edc7fd3dd625c7740afe60ee4f61
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-07 07:56:20 +0000

    Update CamelGames.cs

--- Advent-of-tdd-2023/CamelGames.cs
+++ Advent-of-tdd-2023/CamelGames.cs
@@ -11,5 +11,45 @@ namespace AdventOfCodeTDD
     public List<string> hands=new List<string>();
     public List<int> bids=new List<int>();
     public List<CamelGames> hands=new List<CamelGames>();
-  }
+
+  public void ReadData(string FileName)
+    {
+        string file=@"C:\Downloads\TDD\Day7\"+FileName";
+        if(File.Exist(file))
+        {
+            string[] allLines=File.ReadAllLines(file);
+            foreach(var line in allLines)
+            {
+              String[] splitted=line.Split(" ");
+              foreach(string value in splitted)
+              {
+                if(!string.IsNullOrEmpty(value))
+                {
+                  if(!Regex.IsMatch(value,@"^[0-9]+$"))
+                  {
+                    hands.Add(value);
+                  }
+                  else
+                  {
+                    int i=int.Parse(value);
+                    bids.Add(i);
+                  }
+                }
+              }
+            }
+
+          for(int i=0;i<bids.Count;i++)
+          {
+            CamelGames game=new CamelGames();
+            game.bidAmount=bids[i];
+            game.Handvalue=hands[i];
+            games.Add(game);
+          }          
+        }
+        else
+        {
+            throw new FileNotFoundException();
+        }
+    }
+}
 }
