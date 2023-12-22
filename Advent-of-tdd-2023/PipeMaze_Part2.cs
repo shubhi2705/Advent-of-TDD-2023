1846798c80216b28b5b56b28300de0c87bf4b9f1
Shubhi Singhal <90447402+shubhi2705@users.noreply.github.com>
2023-12-22 15:34:50 +0000

    Create PipeMaze_Part2.cs

---
+++ Advent-of-tdd-2023/PipeMaze_Part2.cs
@@ -0,0 +1,169 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipeMaze
+{
+    public class PipeMaze_Part1
+    {
+        public PipeMaze_Part1()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            string file = Console.ReadLine().Trim();
+            var obj = new PipeMaze_Part1();
+            int value = obj.TotalSteps(file);
+            Console.WriteLine(value);
+        }
+
+        public int TotalSteps(string file)
+        {
+            int value = 0;
+            if (File.Exists(file))
+            {
+                List<string> grid = new List<string>(File.ReadAllLines(file));//read each row in file
+                int sr, sc;
+
+                CordinatesOfStartPosition(grid, out sr, out sc);
+
+                Console.WriteLine(grid[sr][sc]);
+
+                HashSet<(int, int)> TotalVisitedTiles = TotalVisitedCordinates(grid, sr, sc);
+
+                value = TotalVisitedTiles.Count / 2;
+
+                //total tiles enclosed by the loop
+                return TotalTilesEnclosed(grid, TotalVisitedTiles);
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+
+        //finding intersects by ray casting algorithm
+        public int TotalTilesEnclosed(List<string> grid, HashSet<(int, int)> visitedPipes)
+        {
+            int result = 0;
+            //Raycasting alogorithm
+            for (int r = 0; r < grid.Count; r++)
+            {
+                for (int c = 0; c < grid[0].Length; c++)
+                {
+                    if (!visitedPipes.Contains((r, c)))//intersect points must not be in visitedlist
+                    {
+                        int area = CalculateIntersectsPonits(r, c, grid, visitedPipes);
+                        if (area % 2 == 1)
+                        {
+                            result += 1;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public int CalculateIntersectsPonits(int r, int c, List<string> grid, HashSet<(int, int)> visitedPipes)
+        {
+            string s = "LJ|";
+            string line = grid[r];
+            int count = 0;
+            for (int k = 0; k < c; k++)
+            {
+                if (!visitedPipes.Contains((r, k)))
+                {
+                    continue;
+                }
+                else
+                {
+                    if (s.Contains(line[k]))//count intersects
+                        count++;
+                }
+            }
+            return count;
+        }
+        public void CordinatesOfStartPosition(List<string> grid, out int sr, out int sc)
+        {
+            sr = -1;
+            sc = -1;
+            //find the start position
+            for (int r = 0; r < grid.Count; r++)
+            {
+                string row = grid[r];
+                for (int c = 0; c < row.Length; c++)
+                {
+                    char ch = row[c];
+                    if (ch == 'S')
+                    {
+                        sr = r;
+                        sc = c;
+                        break;
+                    }
+                }
+                if (sr != -1 && sc != -1)//break the outer loop once cordinates of start position is found 
+                {
+                    break;
+                }
+            }
+        }
+
+        public HashSet<(int,int)> TotalVisitedCordinates(List<string> grid, int sr, int sc)
+        {
+
+            string TopList = "S|LJ"; //Top
+            string CanUpward = "|7F";
+            string BottomList = "S|7F";
+            string CanDownward = "|LJ";
+            string LeftList = "S-J7";
+            string CanLeft = "-LF";
+            string RightList = "S-LF";
+            string CanRight = "-J7";
+
+            HashSet<(int, int)> visitedPipes = new HashSet<(int, int)>();
+            Queue<(int, int)> q = new Queue<(int, int)>();
+            visitedPipes.Add((sr, sc));//move start S to visitedPipes (which is visited already)
+            q.Enqueue((sr, sc));//add the visited tile to the queue
+
+            while (q.Count > 0)
+            {
+                (int r, int c) = q.Dequeue();//remove visited tile from begining of the queue 
+                char ch = grid[r][c];
+                if (r > 0 && TopList.Contains(ch) && CanUpward.Contains(grid[r - 1][c]) && !visitedPipes.Contains((r - 1, c)))
+                {
+                    visitedPipes.Add((r - 1, c));
+                    q.Enqueue((r - 1, c));
+                }
+                if (grid.Count - 1 > r && BottomList.Contains(ch) && CanDownward.Contains(grid[r + 1][c]) && !visitedPipes.Contains((r + 1, c)))
+                {
+                    visitedPipes.Add((r + 1, c));
+                    q.Enqueue((r + 1, c));
+                }
+                if (c > 0 && LeftList.Contains(ch) && CanLeft.Contains(grid[r][c - 1]) && !visitedPipes.Contains((r, c - 1)))
+                {
+                    visitedPipes.Add((r, c - 1));
+                    q.Enqueue((r, c - 1));
+                }
+                if (grid[r].Length - 1 > c && RightList.Contains(ch) && CanRight.Contains(grid[r][c + 1]) && !visitedPipes.Contains((r, c + 1)))
+                {
+                    visitedPipes.Add((r, c + 1));
+                    q.Enqueue((r, c + 1));
+                }
+            }
+            return visitedPipes;
+        }
+
+        //| is a vertical pipe connecting north and south.
+        //- is a horizontal pipe connecting east and west.
+        //L is a 90-degree bend connecting north and east.
+        //J is a 90-degree bend connecting north and west.
+        //7 is a 90-degree bend connecting south and west.
+        //F is a 90-degree bend connecting south and east.
+        //. is ground; there is no pipe in this tile.
+        //S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+    }
+}
