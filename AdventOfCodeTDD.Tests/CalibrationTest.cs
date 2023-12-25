8c3dddfb0e5b8d52052f547ad402b3725b528cff
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-25 22:07:31 +0530

    Update CalibrationTest.cs

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -5,15 +5,49 @@ namespace AdventOfCodeTDD.Tests
     [TestFixture]
     public class CalibrationTest
     {
-        [Test]
-        public void Test1()
-        {
-            
-        }
-        [Test]
-        public void Test2()
-        {
+        [SetUp]
+public void Setup()
+{
+   
+}
 
-        }
+
+[Test]
+public void FileFound()
+{
+    Parabolic check = new Parabolic();
+    check.FetchInput("testCode.txt");
+    Assert.Pass();
+}
+[Test]
+public void FindRightPart1Value()
+{
+    Parabolic check = new Parabolic();     
+    var data = check.PartOne("testCode.txt");
+    Assert.AreEqual(136, data);
+}
+[Test]
+public void FindWrongPart1Value()
+{
+    Parabolic check = new Parabolic();
+    var data = check.PartOne("testCode.txt");
+    Assert.AreNotEqual(45, data);
+}
+
+[Test]
+public void FindRightPart2Value()
+{
+    Parabolic check = new Parabolic();
+    var data = check.PartTwo("testCode.txt");
+    Assert.AreEqual(64, data);
+}
+[Test]
+public void FindWrongPart2Value()
+{
+    Parabolic check = new Parabolic();
+    var data = check.PartTwo("testCode.txt");
+    Assert.AreNotEqual(320, data);
+}
+        
     }
 }
