53de79264970ce59cf6666ff8b3426a1b4cc9f16
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-25 21:43:44 +0530

    Update CalibrationTest.cs

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -5,56 +5,73 @@ namespace AdventOfCodeTDD.Tests
     [TestFixture]
     public class CalibrationTest
     {
-         private List<string, int> lst;
-
- [SetUp]
- public void Setup()
- {
-     lst = new List<string, int>();
-     lst.Add("pld", 1);
-     lst.Add("bhv", 7);
- }
+ 
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
 
 
- [Test]
- public void FileFound()
- {
-     CodeCheck check = new CodeCheck();
-     check.ReadData("code.txt");
-     Assert.Pass();
- }
- [Test]
- public void FindRightHashValue()
- {
-     CodeCheck check = new CodeCheck();
-     int data = check.Hash("rn=1");
-     Assert.AreEqual(30, data);
- }
- [Test]
- public void FindWrongHashValue()
- {
-     CodeCheck check = new CodeCheck();
-     int data = check.Hash("rn=1");
-     Assert.AreNotEqual(45, data);
- }
- [Test]
- public void GetFocusCorrectValue()
- {
+        [Test]
+        public void FileFound()
+        {
+            CodeCheck check = new CodeCheck();
+            check.ReadData("testCode.txt");
+            Assert.Pass();
+        }
+        [Test]
+        public void FindRightHashValue()
+        {
+            CodeCheck check = new CodeCheck();
+            int data = check.Hash("rn=1");
+            Assert.AreEqual(30, data);
+        }
+        [Test]
+        public void FindWrongHashValue()
+        {
+            CodeCheck check = new CodeCheck();
+            int data = check.Hash("rn=1");
+            Assert.AreNotEqual(45, data);
+        }
 
-     CodeCheck check = new CodeCheck();
-     Dictionary<int, List<(string, int)>> boxes = new Dictionary<int, List<(string, int)>>();
-     int a = check.CalcFocusPower(boxes.Add(0, lst));
-     Assert.AreEqual(15, a);
- }
+        [Test]
+        public void FindRightSumValue()
+        {
+            CodeCheck check = new CodeCheck();
+            check.ReadData("testCode.txt");
+            int data = check.GetSum();
+            Assert.AreEqual(327, data);
+        }
+        [Test]
+        public void FindWrongSumValue()
+        {
+            CodeCheck check = new CodeCheck();
+            check.ReadData("testCode.txt");
+            int data = check.GetSum();
+            Assert.AreNotEqual(320, data);
+        }
+        [Test]
+        public void GetFocusCorrectValue()
+        {
+            string data = " vjx = 2,nvc = 5,xvcn = 4,hf -,ggsh = 6";
+            string[] value = data.Trim().Split(",");
+            CodeCheck check = new CodeCheck();         
+            int a = check.GetPart2Sum(value);
+            Assert.AreEqual(1590, a);
 
- [Test]
- public void GetFocusWrongValue()
- {
+           
+        }
 
-     CodeCheck check = new CodeCheck();
-     Dictionary<int, List<(string, int)>> boxes = new Dictionary<int, List<(string, int)>>();
-     int a = check.CalcFocusPower(boxes.Add(0, lst));
-     Assert.AreEqual(17, a);
- }
+        [Test]
+        public void GetFocusWrongValue()
+        {
+
+            string data = " vjx = 2,nvc = 5,xvcn = 4,hf -,ggsh = 6";
+            string[] value = data.Trim().Split(",");
+            CodeCheck check = new CodeCheck();
+            int a = check.GetPart2Sum(value);
+            Assert.AreNotEqual(1500, a);
+        }
     }
 }
