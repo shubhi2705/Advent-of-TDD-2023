a0af328a852af35e87c999fd333cc1c31a69c29a
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-25 21:29:50 +0530

    Update CalibrationTest.cs

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -5,15 +5,68 @@ namespace AdventOfCodeTDD.Tests
     [TestFixture]
     public class CalibrationTest
     {
-        [Test]
-        public void Test1()
-        {
-            
-        }
-        [Test]
-        public void Test2()
-        {
+       [Test]
+public void GetCoordinatesRight()
+{
+    Destination check = new Destination();
+    var data = @"...#......
+                .......#..";
+    var a = check.ParseGalaxyCoords(data);
+    Assert.AreEqual(2,a.Count());
+}
 
-        }
+[Test]
+public void GetCoordinatesWrong()
+{
+    Destination check = new Destination();
+    var data = @"...#......
+                .......#..
+                ...#...#..";
+    var a = check.ParseGalaxyCoords(data);
+    Assert.AreNotEqual(3, a.Count());
+}
+
+[Test]
+public void SolvePart1Right()
+{
+    Destination check = new Destination();
+    var data = @"...#......
+                .......#..";
+    var a = check.SolvePart1(data);
+    Assert.AreEqual(56, a);
+}
+
+[Test]
+public void SolvePart1Wrong()
+{
+    Destination check = new Destination();
+    var data = @"...#......
+                .......#..
+                ...#...#..";
+    var a = check.SolvePart1(data);
+    Assert.AreNotEqual(170, a);
+}
+
+[Test]
+public void SolvePart2Right()
+{
+    Destination check = new Destination();
+    var data = @"...#......
+                .......#..";
+    var a = check.SolvePart2(data);
+    Assert.AreEqual(27000002, a);
+}
+
+[Test]
+public void SolvePart2Wrong()
+{
+    Destination check = new Destination();
+    var data = @"...#......
+                .......#..
+                ...#...#..";
+    var a = check.SolvePart2(data);
+    Assert.AreNotEqual(81000012, a);
+}
+
     }
 }
