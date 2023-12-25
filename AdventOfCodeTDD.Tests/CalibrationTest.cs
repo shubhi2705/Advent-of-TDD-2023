479ba3fa43770f087462bb4f81de8d47b7ea1a22
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-25 21:05:16 +0530

    Update CalibrationTest.cs

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -5,15 +5,56 @@ namespace AdventOfCodeTDD.Tests
     [TestFixture]
     public class CalibrationTest
     {
-        [Test]
-        public void Test1()
-        {
-            
-        }
-        [Test]
-        public void Test2()
-        {
+         private List<string, int> lst;
 
-        }
+ [SetUp]
+ public void Setup()
+ {
+     lst = new List<string, int>();
+     lst.Add("pld", 1);
+     lst.Add("bhv", 7);
+ }
+
+
+ [Test]
+ public void FileFound()
+ {
+     CodeCheck check = new CodeCheck();
+     check.ReadData("code.txt");
+     Assert.Pass();
+ }
+ [Test]
+ public void FindRightHashValue()
+ {
+     CodeCheck check = new CodeCheck();
+     int data = check.Hash("rn=1");
+     Assert.AreEqual(30, data);
+ }
+ [Test]
+ public void FindWrongHashValue()
+ {
+     CodeCheck check = new CodeCheck();
+     int data = check.Hash("rn=1");
+     Assert.AreNotEqual(45, data);
+ }
+ [Test]
+ public void GetFocusCorrectValue()
+ {
+
+     CodeCheck check = new CodeCheck();
+     Dictionary<int, List<(string, int)>> boxes = new Dictionary<int, List<(string, int)>>();
+     int a = check.CalcFocusPower(boxes.Add(0, lst));
+     Assert.AreEqual(15, a);
+ }
+
+ [Test]
+ public void GetFocusWrongValue()
+ {
+
+     CodeCheck check = new CodeCheck();
+     Dictionary<int, List<(string, int)>> boxes = new Dictionary<int, List<(string, int)>>();
+     int a = check.CalcFocusPower(boxes.Add(0, lst));
+     Assert.AreEqual(17, a);
+ }
     }
 }
