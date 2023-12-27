6685b4c42a3640b353c5469ed268e45eeb4544cd
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-27 21:31:59 +0530

    Update CalibrationTest.cs

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -5,83 +5,59 @@ namespace AdventOfCodeTDD.Tests
     [TestFixture]
     public class CalibrationTest
     {
-        [Test]
-        public void check_whether_given_input_is_FiveOfaKind_type()
-        {
-            
-        }
-        [Test]
-        public void check_whether_given_input_is_FourOfaKind_type()
-        {
+           [SetUp]
+    public void Setup()
+    {
+       
+    }
 
-        }
-        [Test]
-        public void check_whether_given_input_is_FullHouse_type()
-        {
+    [Test]
+    public void ParseGamesRight()
+    {
+        Game check = new Game();
+        var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
+        var data = check.ParseGames(input);
+        Assert.AreEqual(5, data.Count);
+    }
+    [Test]
+    public void ParseGamesWrong()
+    {
+        Game check = new Game();
+        var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
+        var data = check.ParseGames(input);
+        Assert.AreNotEqual(3, data.Count);
+    }
 
-        }
-        [Test]
-        public void check_whether_given_input_is_ThreeOfAKind_type()
-        {
+    [Test]
+    public void FindRightPart1Value()
+    {
+        Game check = new Game();     
+        var data = check.SolvePart1("testCode.txt");
+        Assert.AreEqual(6440, data);
+    }
+    [Test]
+    public void FindWrongPart1Value()
+    {
+        Game check = new Game();
+        var data = check.SolvePart1("testCode.txt");
+        Assert.AreNotEqual(5000, data);
+    }
 
-        }
-        [Test]
-        public void check_whether_given_input_is_TwoPair_type()
-        {
-
-        }
-        [Test]
-        public void check_whether_given_input_is_OnePair_type()
-        {
-
-        }
-        [Test]
-        public void check_whether_given_input_is_HighCard_type()
-        {
-
-        }
-
-        [Test]
-        public void check_whether_33332_is_stronger_than_2AAAAA()
-        {
-
-        }
-
-        [Test]
-        public void check_whether_77888_is_stronger_than_77788()
-        {
-
-        }
-
-        [Test]
-        public void check_ranks_of_given_input_32T3K_T55J5()
-        {
-
-        }
-
-        [Test]
-        public void check_whether_input_is_empty()
-        {
-
-        }
-
-        [Test]
-        public void check_whether_input_is_invalid()
-        {
-
-        }
-
-        [Test]
-        public void calculate_bid_rank_multiplication_for_given_input()
-        {
-
-        }
-
-        [Test]
-        public void calculate_total_winning_for_given_input()
-        {
-
-        }
+    [Test]
+    public void FindRightPart2Value()
+    {
+        Game check = new Game();
+        var data = check.SolvePart1("testCode.txt");
+        Assert.AreEqual(6440, data);
+    }
+    [Test]
+    public void FindWrongPart2Value()
+    {
+        Game check = new Game();
+        var data = check.SolvePart1("testCode.txt");
+        Assert.AreNotEqual(4000, data);
+    }
+    
 
     }
 }
