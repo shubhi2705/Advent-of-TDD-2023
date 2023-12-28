c512875da73d2d5d54c59e5cffc1495da8f95597
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-28 17:15:28 +0530

    Update CalibrationTest.cs

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -6,38 +6,38 @@ namespace AdventOfCodeTDD.Tests
     public class CalibrationTest
     {
         [Test]
- public void FindRightPart1Value()
- {
-     var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
-     Counter counter = new Counter(input);   
-     var data = counter.Part1();
-     Assert.AreEqual(42, data);
- }
+         public void FindRightPart1Value()
+         {
+             var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
+             Counter counter = new Counter(input);   
+             var data = counter.Part1();
+             Assert.AreEqual(42, data);
+         }
 
- [Test]
- public void FindWrongPart1Value()
- {
-     var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
-     Counter counter = new Counter(input);
-     var data = counter.Part1();
-     Assert.AreNotEqual(40, data);
- }
+         [Test]
+         public void FindWrongPart1Value()
+         {
+             var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
+             Counter counter = new Counter(input);
+             var data = counter.Part1();
+             Assert.AreNotEqual(40, data);
+         }
 
- [Test]
- public void FindRightPart2Value()
- {
-     var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
-     Counter counter = new Counter(input);
-     var data = counter.Part2();
-     Assert.AreEqual(435323968681713, data);
- }
- [Test]
- public void FindWrongPart2Value()
- {
-     var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
-     Counter counter = new Counter(input);
-     var data = counter.Part2();
-     Assert.AreNotEqual(80000, data);
- }
+         [Test]
+         public void FindRightPart2Value()
+         {
+             var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
+             Counter counter = new Counter(input);
+             var data = counter.Part2();
+             Assert.AreEqual(435323968681713, data);
+         }
+         [Test]
+         public void FindWrongPart2Value()
+         {
+             var input = File.ReadAllText(@"C:\Users\MSUSERSL123\Documents\Data\testCode.txt");
+             Counter counter = new Counter(input);
+             var data = counter.Part2();
+             Assert.AreNotEqual(80000, data);
+         }
     }
 }
