71a055967b0e8ab16439d36eec5de2c6d7312265
nav9996 <152150365+nav9996@users.noreply.github.com>
2023-12-29 15:03:59 +0530

    Update CalibrationTest.cs

--- AdventOfCodeTDD.Tests/CalibrationTest.cs
+++ AdventOfCodeTDD.Tests/CalibrationTest.cs
@@ -6,14 +6,54 @@ namespace AdventOfCodeTDD.Tests
     public class CalibrationTest
     {
         [Test]
-        public void Test1()
-        {
-            
-        }
-        [Test]
-        public void Test2()
-        {
+public void FindRightPart1Value()
+{
+    var input = @"19, 13, 30 @ -2,  1, -2
+                18, 19, 22 @ -1, -1, -2
+                20, 25, 34 @ -2, -2, -4
+                12, 31, 28 @ -1, -2, -1
+                20, 19, 15 @  1, -5, -3";
+    Hailstones counter = new Hailstones(input);   
+    var data = counter.Part1();
+    Assert.AreEqual(0, data);
+}
+       
+[Test]
+public void FindWrongPart1Value()
+{
+    var input = @"19, 13, 30 @ -2,  1, -2
+                18, 19, 22 @ -1, -1, -2
+                20, 25, 34 @ -2, -2, -4
+                12, 31, 28 @ -1, -2, -1
+                20, 19, 15 @  1, -5, -3";
+    Hailstones counter = new Hailstones(input);
+    var data = counter.Part1();
+    Assert.AreNotEqual(2, data);
+}
 
-        }
+[Test]
+public void FindRightPart2Value()
+{
+    var input = @"19, 13, 30 @ -2,  1, -2
+                18, 19, 22 @ -1, -1, -2
+                20, 25, 34 @ -2, -2, -4
+                12, 31, 28 @ -1, -2, -1
+                20, 19, 15 @  1, -5, -3";
+    Hailstones counter = new Hailstones(input);
+    var data = counter.Part2();
+    Assert.AreEqual(47, data);
+}
+[Test]
+public void FindWrongPart2Value()
+{
+    var input = @"19, 13, 30 @ -2,  1, -2
+                18, 19, 22 @ -1, -1, -2
+                20, 25, 34 @ -2, -2, -4
+                12, 31, 28 @ -1, -2, -1
+                20, 19, 15 @  1, -5, -3";
+    Hailstones counter = new Hailstones(input);
+    var data = counter.Part2();
+    Assert.AreNotEqual(45, data);
+}
     }
 }
